c3dc638af0c0f129b06a55693c71f4ce4686005a
haneenaridi <haridi@montgomerycollege.edu>
2024-06-21 20:12:10 -0400

    setup

--- src/main/java/com/antneen/mirrorDimension/item/ModToolTiers.java
+++ src/main/java/com/antneen/mirrorDimension/item/ModToolTiers.java
@@ -1,8 +1,9 @@
 package com.antneen.mirrorDimension.item;
 
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.Tier;
 import net.minecraftforge.common.TierSortingRegistry;
 
 public class ModToolTiers {
-    
+    SoundEvents.ARMOR_EQUIP_GOLD
 }
